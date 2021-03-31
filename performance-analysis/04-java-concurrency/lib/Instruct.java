e4e1e84dcda9281bf7148df1666ad89b6a8c4482
farui.yu <farui.yu@kingstarfintech.com>
2021-03-31 10:59:36 +0800

    文档更新

---
+++ performance-analysis/04-java-concurrency/lib/Instruct.java
@@ -0,0 +1,14 @@
+/**
+ * @date: 2021/3/29 14:57
+ * @author: farui.yu
+ */
+public class Instruct {
+
+    // javac Instruct.java
+    // javap -c Instruct.class > instruct
+    public int add() {
+        int i = 2;
+        int j = 3;
+        return i + j;
+    }
+}
